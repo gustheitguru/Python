ad8a1f19063aababdc9760c09cc913540b58e815
gustheitguru <gus@gustheitguru.com>
2015-10-24 11:02:05 -0600

    Python code

---
+++ Quiz.py
@@ -0,0 +1,14 @@
+
+
+print("Here is a quiz to test your knowledge of farming...")
+print()
+print()
+print("Question 1")
+print("What percentage of the land is used for farming?")
+print()
+print("a. 25%")
+print("b. 50%")
+print("c. 75%")
+answer = input("Make your choice: ")
+if answer == "c":
+    print("Correct!")
---
+++ word of the day/numgen.py
@@ -0,0 +1,8 @@
+import random
+
+x = 1
+y = 10
+
+numgen = random.randint(x, y)
+
+print numgen
